fd932d69b774085d5b3eaa48a7524883917b9663
a-v-prasad <alisha.prasad@kellogg.northwestern.edu>
2026-02-11 02:25:34 +0000

    part 1- deleted table code

--- kmdb.rb
+++ kmdb.rb
@@ -77,7 +77,13 @@
 
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
-# TODO!
+# TODO! - completed
+Studio.destroy_all
+Movie.destroy_all
+Actor.destroy_all
+Role.destroy_all
+Agent.destroy_all
+
 
 # Generate models and tables, according to the domain model.
 # TODO!
